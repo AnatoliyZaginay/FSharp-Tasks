3ec2775a6a4c7578f01ec24a3cceff40c31f3b29
AnatoliyZaginay <anatoliy.zaginay@gmail.com>
2022-03-02 20:44:20 +0300

    Added factorial function

---
+++ Tasks/Task_1/Program.fs
@@ -0,0 +1,6 @@
+﻿let factorial number =
+    if number < 0 then failwith "Factorial can't be calculated for a negative number"
+    let rec subFactorial currentNumber acc =
+        if currentNumber = 0 then acc
+        else subFactorial (currentNumber - 1) (acc * currentNumber)
+    subFactorial number 1
