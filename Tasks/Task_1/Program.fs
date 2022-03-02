adc3bde38ccca412e376405ee8ef56a161b0b858
AnatoliyZaginay <anatoliy.zaginay@gmail.com>
2022-03-02 21:29:15 +0300

    Added fibonacci function

--- Tasks/Task_1/Program.fs
+++ Tasks/Task_1/Program.fs
@@ -4,3 +4,11 @@
         if currentNumber = 0 then acc
         else subFactorial (currentNumber - 1) (acc * currentNumber)
     subFactorial number 1
+
+let fibonacci number =
+    if number < 0 then failwith "Element of Fibonacci sequence can't be calculated for a negative number"
+    let rec subFibonacci firstNumber secondNumber i =
+        if i = number then secondNumber
+        else subFibonacci secondNumber (firstNumber + secondNumber) (i + 1)
+    if number = 0 then 0
+    else subFibonacci 0 1 1
