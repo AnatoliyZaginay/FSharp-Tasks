cfda33302fb522de8763d7d6859a614f7d708042
AnatoliyZaginay <anatoliy.zaginay@gmail.com>
2022-03-10 21:22:35 +0300

    Added mapBinaryTree function

---
+++ Tasks/Task_2/BinaryTree.fs
@@ -0,0 +1,10 @@
+﻿module BinaryTree
+
+type BinaryTree<'a> =
+| BinaryTree of 'a * BinaryTree<'a> * BinaryTree<'a>
+| Leaf of 'a
+
+let rec mapBinaryTree func binaryTree =
+    match binaryTree with
+    | Leaf(value) -> Leaf(func value)
+    | BinaryTree(value, leftSubTree, rightSubTree) -> BinaryTree(func value, mapBinaryTree func leftSubTree, mapBinaryTree func rightSubTree)
