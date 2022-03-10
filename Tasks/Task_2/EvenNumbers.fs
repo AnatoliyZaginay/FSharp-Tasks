500e1871c39adb3aeb640ffcb27f9cdb1f63451e
AnatoliyZaginay <anatoliy.zaginay@gmail.com>
2022-03-10 21:05:33 +0300

    Added CountEvenNumbers functions

---
+++ Tasks/Task_2/EvenNumbers.fs
@@ -0,0 +1,10 @@
+﻿module EvenNumbers
+
+let CountEvenNumbersViaMap list =
+    list |> List.map(fun x -> abs(x + 1) % 2) |> List.sum
+
+let CountEvenNumbersViaFilter list =
+    list |> List.filter(fun x -> x % 2 = 0) |> List.length
+
+let CountEvenNumbersViaFold list =
+    list |> List.fold(fun acc x -> acc + abs(x + 1) % 2) 0
