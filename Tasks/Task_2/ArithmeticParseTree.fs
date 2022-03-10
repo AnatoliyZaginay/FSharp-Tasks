7667da999fde2a666d837c122445857bac0b82ab
AnatoliyZaginay <anatoliy.zaginay@gmail.com>
2022-03-10 22:00:59 +0300

    Added calculateArithmeticParseTree function

---
+++ Tasks/Task_2/ArithmeticParseTree.fs
@@ -0,0 +1,23 @@
+﻿module ArithmeticParseTree
+
+type ArithmeticParseTree =
+| Leaf of int
+| Tree of Operation * ArithmeticParseTree * ArithmeticParseTree
+and Operation =
+| Sum
+| Subtraction
+| Multiplication
+| Division
+
+let rec calculateArithmeticParseTree arithmeticParseTree =
+    match arithmeticParseTree with
+    | Leaf(value) -> value
+    | Tree(operation, leftSubTree, rightSubTree) ->
+        match operation with
+        | Sum -> calculateArithmeticParseTree(leftSubTree) + calculateArithmeticParseTree(rightSubTree)
+        | Subtraction -> calculateArithmeticParseTree(leftSubTree) - calculateArithmeticParseTree(rightSubTree)
+        | Multiplication -> calculateArithmeticParseTree(leftSubTree) * calculateArithmeticParseTree(rightSubTree)
+        | Division ->
+            let divider = calculateArithmeticParseTree(rightSubTree)
+            if divider = 0 then failwith "You can't divide by zero"
+            calculateArithmeticParseTree(leftSubTree) / divider
